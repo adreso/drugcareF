09ea85234a5258c9563b566438b75a13a89a3b27
adreso <arrieta10andres@gmail.com>
2020-03-06 13:19:21 -0500

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//Rutas
+import { APP_ROUTES } from './app.routes';
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MainpageComponent } from './pages/mainpage.component';
+import { LoginComponent } from './login/login.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { PagesComponent } from './pages/pages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MainpageComponent,
+    LoginComponent,
+    NopagefoundComponent,
+    DashboardComponent,
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbsComponent,
+    FooterComponent,
+    PagesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTES
   ],
   providers: [],
   bootstrap: [AppComponent]
