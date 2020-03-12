c7ca7e4e1bed0c9ac9f2be849831c073ceb841ce
aarrieta <andres.inf@zayma.com>
2020-03-11 19:07:58 -0500

    Menu

--- src/app/services/sidebar.service.ts
+++ src/app/services/sidebar.service.ts
@@ -6,4 +6,18 @@ import { Injectable } from '@angular/core';
 export class SidebarService {
 
   constructor() { }
+
+  menu: any = [
+    {
+      titulo: 'principal',
+      icono:'nav-icon fas fa-tachometer-alt',
+      submenu:[
+        {
+          titulo:'Dashboard',
+          url:'/dashboard'
+
+        }
+      ]
+    }
+  ]
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
 import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -13,7 +16,8 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 
   ],
   imports: [
-
+    RouterModule,
+    CommonModule
   ],
   exports: [
     BreadcrumbsComponent,
